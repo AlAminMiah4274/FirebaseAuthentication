6465867aa6f5c2147167e0de311f6ef7f2eff610
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-02-22 21:23:19 +0600

    learnt authentication with google

---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC2zdkE0rhHyc94IuKt_6LD6DRHomtjZYw",
+    authDomain: "simple-firebase-authenti-7e9d3.firebaseapp.com",
+    projectId: "simple-firebase-authenti-7e9d3",
+    storageBucket: "simple-firebase-authenti-7e9d3.appspot.com",
+    messagingSenderId: "746724375009",
+    appId: "1:746724375009:web:74d14cc38cf4e84843baa0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
